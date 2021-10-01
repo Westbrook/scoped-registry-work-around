bba43cb62b61c5f29b2cf2655283e64cb5734ca9
Manuel Martin <manuel.martin@gmail.com>
2021-10-01 10:45:23 +0200

    fix: test using @open-wc/testing

--- test/another-test.test.ts
+++ test/another-test.test.ts
@@ -1,24 +1,25 @@
+import {fixture, elementUpdated, expect} from '@open-wc/testing';
 import '@spectrum-web-components/card/sp-card.js';
-import { Card } from '@spectrum-web-components/card';
+import {Card} from '@spectrum-web-components/card';
 import '@spectrum-web-components/action-menu/sp-action-menu.js';
 import '@spectrum-web-components/menu/sp-menu.js';
 import '@spectrum-web-components/menu/sp-menu-item.js';
 import '@spectrum-web-components/menu/sp-menu-divider.js';
-import { fixture, elementUpdated, html, expect } from '@open-wc/testing';
 
 import {
-    smallHorizontal,
-    StoryArgs,
+  smallHorizontal,
+  StoryArgs,
 } from '../stories/index.js';
+
 
 describe('AnotherTest', () => {
   it.only('loads - [horizontal]', async () => {
     const el = await fixture<Card>(
-        smallHorizontal(smallHorizontal.args as StoryArgs)
+      smallHorizontal(smallHorizontal.args as StoryArgs)
     );
 
     await elementUpdated(el);
 
     await expect(el).to.be.accessible();
-});
+  });
 });
